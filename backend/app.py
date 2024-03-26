cb9b6578644cefd3f440791a81351486a92e8fab
William Leung <will.leung2022@gmail.com>
2024-03-26 13:20:34 -0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,6 @@ from flask import Flask, render_template, request
 from flask_cors import CORS
 from helpers.MySQLDatabaseHandler import MySQLDatabaseHandler
 import pandas as pd
-import sklearn
 #from sklearn.feature_extraction.text import TfidfVectorizer
 #from sklearn.metrics.pairwise import cosine_similarity
 
